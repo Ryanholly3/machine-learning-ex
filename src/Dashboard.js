8ffc18c0a3ea49c948a225e755e77ca99e5ba203
ryanholly3 <ryanholly3@gmail.com>
2019-02-12 16:50:48 -0700

    set up commit

---
+++ src/Dashboard.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './App.css';
+
+
+class Dashboard extends Component {
+  render() {
+    return (
+      <div className="dashboard">
+        <h1>Machine Learning Examples</h1>
+      </div>
+    );
+  }
+}
+
+export default Dashboard;
