e8ba9b0d09ca448d0a5a5f4dbf91a88fec498d48
ryanholly3 <ryanholly3@gmail.com>
2019-02-15 15:06:47 -0700

    added first tensor

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -4,6 +4,18 @@ import * as tf from '@tensorflow/tfjs';
 
 
 class Dashboard extends Component {
+
+  componentDidMount(){
+
+    const values = [];
+    for(let i = 0; i < 100;i++){
+      values[i] = i
+    }
+    const shape = [10, 10]
+    const a = tf.tensor2d(values, shape, 'int32')
+    a.print()
+  }
+
   render() {
     return (
       <div className="dashboard">
