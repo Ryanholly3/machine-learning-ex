21e0d6be609144a375e30588e0e7efe5ee1068d1
ryanholly3 <ryanholly3@gmail.com>
2019-02-18 17:33:03 -0700

    decided on example

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -9,7 +9,7 @@ class Dashboard extends Component {
 
     const values = [];
     for(let i = 0; i < 100;i++){
-      values[i] = i
+      values[i]
     }
     const shape = [10, 10]
     const a = tf.tensor2d(values, shape, 'int32')
@@ -19,7 +19,7 @@ class Dashboard extends Component {
   render() {
     return (
       <div className="dashboard">
-        <h1>Machine Learning Examples</h1>
+        <h1>Learn that Face</h1>
       </div>
     );
   }
